6af8e685c29031e5e8712dff47327eb6262bb2c4
Shmuel Lotman <shmuel.lotman@gmail.com>
2018-01-08 17:17:05 -0500

    user and order

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,14 @@
 const User = require('./user')
-
+const Product = require('./product')
+const Review = require('./review')
+const Order = require('./order')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
  *
  *    BlogPost.belongsTo(User)
  */
-
+Order.belongsTo(User)
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -14,5 +16,8 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Product,
+  Review,
+  Order
 }
