6472a3d9b053d09ceaeaae65598d96fdde1f12df
Suyash Chopra <suyashchopra19@gmail.com>
2018-01-11 12:23:11 -0500

    api routing - johns notes

--- server/api/index.js
+++ server/api/index.js
@@ -6,9 +6,6 @@ router.use('/orders', require('./order'))
 router.use('/reviews', require('./review'))
 router.use('/products', require('./products'))
 router.use('/lineItems', require('./lineItem') )
-// JM - make the route /produtcs
-//router.use('/', require('./products'))
-
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
--- server/api/products.js
+++ server/api/products.js
@@ -1,24 +1,24 @@
-const {Product, Review} = require('../db/models');
-const app = require('express').Router();
+const { Product, Review } = require("../db/models");
+const app = require("express").Router();
 module.exports = app;
 
-app.get('/', (req, res, next) => {
-    Product.findAll({})
+app.get("/", (req, res, next) => {
+  Product.findAll({})
     .then(products => res.send(products))
-    .catch(next)
-})
-
-app.get('/:id', (req, res, next) => {
-    Product.findById(req.params.id)
-    .then(product => res.send(product))
-    // JM - do you also want to send back all reviews for this product?
-    //  include: [Review]
-    .catch(next)
-})
-
-// Get all reviews made on a single product
-app.get('/:id/reviews', (req,res,next) => {
-    Review.findAll({where: {productId: req.params.id}})
-    .then(reviews => res.json(reviews))
     .catch(next);
-})
+});
+
+app.get("/:id", (req, res, next) => {
+  Product.findById(req.params.id, {
+    include: [
+      {
+        model: Review,
+        where: {
+          productId: req.params.id
+        }
+      }
+    ]
+  })
+    .then(product => res.send(product))
+    .catch(next);
+});
