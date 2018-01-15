903c0dd88d7d5d16c9c6d41b9035b49fdf58bd2d
Shmuel Lotman <shmuel.lotman@gmail.com>
2018-01-15 17:05:54 -0500

    could oauth actually work the first time?!

--- server/auth/google.js
+++ server/auth/google.js
@@ -4,19 +4,6 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const {User} = require('../db/models')
 module.exports = router
 
-/**
- * For OAuth keys and other secrets, your Node process will search
- * process.env to find environment variables. On your production server,
- * you will be able to set these environment variables with the appropriate
- * values. In development, a good practice is to keep a separate file with
- * these secrets that you only share with your team - it should NOT be tracked
- * by git! In this case, you may use a file called `secrets.js`, which will
- * set these environment variables like so:
- *
- * process.env.GOOGLE_CLIENT_ID = 'your google client id'
- * process.env.GOOGLE_CLIENT_SECRET = 'your google client secret'
- * process.env.GOOGLE_CALLBACK = '/your/google/callback'
- */
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 
@@ -34,7 +21,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
     const googleId = profile.id
     const name = profile.displayName
     const email = profile.emails[0].value
-
+    console.log(profile)
     User.find({where: {googleId}})
       .then(foundUser => (foundUser
         ? done(null, foundUser)
@@ -49,7 +36,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   router.get('/', passport.authenticate('google', {scope: 'email'}))
 
   router.get('/callback', passport.authenticate('google', {
-    successRedirect: '/home',
+    successRedirect: '/products',
     failureRedirect: '/login'
   }))
 
